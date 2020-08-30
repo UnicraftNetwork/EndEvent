bd20d97e1d1f1fd9eada2cbbade641f11bad4ff3
BGMP <jose@bgmp.cl>
2020-08-29 21:11:13 -0400

    Gather event winner and end the match upon

--- src/main/java/cl/bgmp/endevent/modules/EnderDragonMatchModule.java
+++ src/main/java/cl/bgmp/endevent/modules/EnderDragonMatchModule.java
@@ -3,14 +3,16 @@ package cl.bgmp.endevent.modules;
 import cl.bgmp.endevent.EndEvent;
 import cl.bgmp.endevent.events.MatchStartEvent;
 import cl.bgmp.endevent.match.Match;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class EnderDragonMatchModule implements MatchModule, Listener {
   private Match match;
@@ -34,13 +36,26 @@ public class EnderDragonMatchModule implements MatchModule, Listener {
     modifyEventDragon();
   }
 
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  public void onDragonDeath(EntityDeathEvent event) {
+    Entity entity = event.getEntity();
+    if (!(entity instanceof EnderDragon)) return;
+
+    EnderDragon enderDragon = (EnderDragon) entity;
+    Player killer = enderDragon.getKiller();
+
+    if (killer != null) {
+      this.match.end("The EndEvent has finished. Winner: " + killer.getDisplayName());
+    } else {
+      this.match.end("The EndEvent has finished. The Dragon died for unknown reasons.");
+    }
+  }
+
   @SuppressWarnings("deprecation")
   private void modifyEventDragon() {
     this.enderDragon.setCustomName(ChatColor.RED + "Event Dragon");
-    Bukkit.broadcastMessage("Vida antes: " + enderDragon.getHealth());
     this.enderDragon.setMaxHealth(enderDragon.getHealth() * 3);
     this.enderDragon.setHealth(enderDragon.getMaxHealth());
-    Bukkit.broadcastMessage("Vida despues: " + enderDragon.getHealth());
 
     if (enderDragon.getBossBar() == null) return;
 
