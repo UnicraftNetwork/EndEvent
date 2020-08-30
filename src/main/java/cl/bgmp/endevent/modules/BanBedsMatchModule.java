698d9f9bd1fc6bd0a5454b2bb7aaf976fdc63778
BGMP <jose@bgmp.cl>
2020-08-29 21:05:45 -0400

    Fix beds ban module

--- src/main/java/cl/bgmp/endevent/modules/BanBedsMatchModule.java
+++ src/main/java/cl/bgmp/endevent/modules/BanBedsMatchModule.java
@@ -2,6 +2,9 @@ package cl.bgmp.endevent.modules;
 
 import cl.bgmp.endevent.EndEvent;
 import cl.bgmp.endevent.match.Match;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,6 +15,25 @@ import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BanBedsMatchModule implements MatchModule, Listener {
   private Match match;
+  private List<Material> beds =
+      ImmutableList.<Material>builder()
+          .add(Material.BLACK_BED)
+          .add(Material.WHITE_BED)
+          .add(Material.ORANGE_BED)
+          .add(Material.LIGHT_BLUE_BED)
+          .add(Material.YELLOW_BED)
+          .add(Material.LIME_BED)
+          .add(Material.PINK_BED)
+          .add(Material.LIGHT_GRAY_BED)
+          .add(Material.CYAN_BED)
+          .add(Material.PURPLE_BED)
+          .add(Material.BLUE_BED)
+          .add(Material.BROWN_BED)
+          .add(Material.GREEN_BED)
+          .add(Material.RED_BED)
+          .add(Material.LEGACY_BED)
+          .add(Material.LEGACY_BED_BLOCK)
+          .build();
 
   public BanBedsMatchModule(Match match) {
     this.match = match;
@@ -23,7 +45,7 @@ public class BanBedsMatchModule implements MatchModule, Listener {
     if (!match.getPlayers().contains(player)) return;
 
     Block block = event.getBlock();
-    if (block.getType().toString().contains("BED")) event.setCancelled(true);
+    if (beds.contains(block.getType())) event.setCancelled(true);
   }
 
   @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -32,7 +54,7 @@ public class BanBedsMatchModule implements MatchModule, Listener {
     if (!match.getPlayers().contains(player)) return;
 
     Block block = event.getBlock();
-    if (block.getType().toString().contains("BED")) event.setCancelled(true);
+    if (beds.contains(block.getType())) event.setCancelled(true);
   }
 
   @Override
